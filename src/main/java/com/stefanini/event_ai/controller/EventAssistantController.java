55305ea27105bdac07de5af5a265ea37db367e63
OtavioRodrrigues365 <otavio.araujo@sptech.school>
2026-05-22 08:39:04 -0300

    ref: mudando forma de implementação da resiliencia

--- src/main/java/com/stefanini/event_ai/controller/EventAssistantController.java
+++ src/main/java/com/stefanini/event_ai/controller/EventAssistantController.java
@@ -1,6 +1,9 @@
 package com.stefanini.event_ai.controller;
 
-import com.stefanini.event_ai.assistant.EventAssistantCircuitBreakerService;
+import com.stefanini.event_ai.assistant.EventAssistantAiService;
+import dev.langchain4j.model.output.TokenUsage;
+import dev.langchain4j.service.Result;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/event/assistant")
 public class EventAssistantController {
 
-    private final EventAssistantCircuitBreakerService eventAssistantCircuitBreakerService;
+    private final EventAssistantAiService eventAssistantAiService;
 
-    public EventAssistantController(EventAssistantCircuitBreakerService eventAssistantCircuitBreakerService) {
-        this.eventAssistantCircuitBreakerService = eventAssistantCircuitBreakerService;
+    public EventAssistantController(EventAssistantAiService eventAssistantAiService) {
+        this.eventAssistantAiService = eventAssistantAiService;
     }
 
 
     @PostMapping()
+    @CircuitBreaker(name = "geminiAssistant", fallbackMethod = "fallbackResponse")
     public String askAssistant(@RequestBody String userMessage) {
-        return eventAssistantCircuitBreakerService.askAssistant(userMessage);
+        Result<String> result = eventAssistantAiService.handleRequest(userMessage);
+
+        TokenUsage tokenUsage = result.tokenUsage();
+        if (tokenUsage != null) {
+            int inputTokens = tokenUsage.inputTokenCount();
+            int outputTokens = tokenUsage.outputTokenCount();
+            System.out.println("Custo mapeado -> Input: " + inputTokens + " | Output: " + outputTokens);
+        }
+
+        return result.content();
+    }
+
+
+    private String fallbackResponse() {
+        return """
+               O assistente está temporariamente indisponível. Tente novamente em alguns instantes.
+                """;
     }
 }
